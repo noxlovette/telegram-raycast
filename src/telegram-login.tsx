98d4cfd2a22e39125e6dc93e4fa58c6d32aa6914
Danila Volkov <123877021+noxlovette@users.noreply.github.com>
2024-06-18 11:37:57 +0200

    polish

--- src/telegram-login.tsx
+++ src/telegram-login.tsx
@@ -1,6 +1,27 @@
 import React, { useState } from "react";
-import { Action, ActionPanel, LocalStorage, Form, Icon, showToast, Toast, getPreferenceValues, launchCommand, LaunchType } from "@raycast/api";
+import {
+  Action,
+  ActionPanel,
+  LocalStorage,
+  Form,
+  Icon,
+  showToast,
+  Toast,
+  getPreferenceValues,
+  launchCommand,
+  LaunchType,
+} from "@raycast/api";
+import { TelegramClient, Api } from "telegram";
+import { StringSession } from "telegram/sessions";
 import got from "got";
+
+interface Contact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber?: string;
+  username: string;
+}
 
 export default function Command() {
   const [isCodeFieldVisible, setIsCodeFieldVisible] = useState(false);
@@ -18,22 +39,12 @@ export default function Command() {
   }
 
   return (
-    
     <Form
-    searchBarAccessory={
-      <Form.LinkAccessory
-        target="https://github.com/noxlovette/telegram-raycast/"
-        text="Open Documentation"
-      />
-    }
-
-    navigationTitle="Telegram Login"
-
-      actions={
-        <ActionPanel>
-          {currentAction}
-        </ActionPanel>
+      searchBarAccessory={
+        <Form.LinkAccessory target="https://github.com/noxlovette/telegram-proxy/" text="Open Proxy Code" />
       }
+      navigationTitle="Telegram Login"
+      actions={<ActionPanel>{currentAction}</ActionPanel>}
     >
       {currentFields}
       <Form.Description
@@ -44,35 +55,37 @@ export default function Command() {
   );
 }
 
-function RequestPhoneCode({ setIsCodeFieldVisible }: { setIsCodeFieldVisible: React.Dispatch<React.SetStateAction<boolean>> }) {
+function RequestPhoneCode({
+  setIsCodeFieldVisible,
+}: {
+  setIsCodeFieldVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   async function handleSubmit() {
-    const {phoneNumber} = getPreferenceValues<ExtensionPreferences>(); // Replace with the actual phone number
+    const { phoneNumber } = getPreferenceValues<ExtensionPreferences>(); // Replace with the actual phone number
 
     const toast = await showToast({
       style: Toast.Style.Animated,
-      title: "Talking to TG",
+      title: "Talking to Proxy",
     });
 
     try {
-      const { body } = await got.post<{ message: string }>("http://localhost:3000/send-code", {
+      const { body } = await got.post<{ message: string }>("https://telray-proxy.up.railway.app/send-code", {
         json: {
           phoneNumber: phoneNumber,
         },
         responseType: "json",
       });
-      console.log(body); // Log the response body
 
       toast.style = Toast.Style.Success;
       toast.title = body.message;
       toast.message = "Please enter the code you received";
 
-      LocalStorage.setItem("phone", phoneNumber); // Save the phone number
-      setIsCodeFieldVisible(true); // Set state to true to render the code field
+      LocalStorage.setItem("phone", phoneNumber);
+      setIsCodeFieldVisible(true);
     } catch (error) {
       toast.style = Toast.Style.Failure;
       toast.title = "Failed sending to proxy";
       toast.message = String(error);
-      console.log(error);
     }
   }
 
@@ -80,7 +93,7 @@ function RequestPhoneCode({ setIsCodeFieldVisible }: { setIsCodeFieldVisible: Re
 }
 
 function SendPhoneCode() {
-  async function handleSubmit(values: { code: string, password: string}) {
+  async function handleSubmit(values: { code: string; password: string }) {
     if (!values.code) {
       showToast({
         style: Toast.Style.Failure,
@@ -102,9 +115,9 @@ function SendPhoneCode() {
     });
 
     try {
-      const { body } = await got.post("http://localhost:3000/start-client", {
+      const { body } = await got.post("https://telray-proxy.up.railway.app/start-client", {
         json: {
-          phoneNumber: await LocalStorage.getItem("phone"), // Get the phone number from local storage
+          phoneNumber: await LocalStorage.getItem("phone"),
           password: values.password || "",
           phoneCode: values.code,
         },
@@ -114,14 +127,34 @@ function SendPhoneCode() {
       toast.style = Toast.Style.Success;
       toast.title = "Logged in";
       toast.message = "Successfully started client";
-      await LocalStorage.clear(); // Clear the phone number and phone code
-      await LocalStorage.setItem("savedSession", body.savedSession); // save the session string for later use
-      await launchCommand({name: "telegram-send-message", type: LaunchType.UserInitiated, context: {message: "You are all set!"}});
+      await LocalStorage.clear();
+      await LocalStorage.setItem("savedSession", body.savedSession);
+
+      const SESSION = new StringSession(body.savedSession); // might be redundant but it works
+      const client = new TelegramClient(SESSION, 12345678, "api_hash", { connectionRetries: 5 });
+      await client.connect();
+      const contacts = await client.invoke(new Api.contacts.GetContacts({}));
+      if (contacts instanceof Api.contacts.Contacts) {
+        const usersList: Contact[] = contacts.users.map((user) => ({
+          id: user.id.toString(),
+          firstName: user.firstName || "",
+          lastName: user.lastName || "",
+          phoneNumber: user.phone || "",
+          username: user.username || "",
+        }));
+
+        LocalStorage.setItem("contacts", JSON.stringify(usersList));
+      }
+      await launchCommand({
+        name: "telegram-send-message",
+        type: LaunchType.UserInitiated,
+        context: { message: "You are all set!" },
+      });
     } catch (error) {
       toast.style = Toast.Style.Failure;
       toast.title = "Failed to start client";
-      toast.message = String(error);
-      console.log(error);
+      toast.message = "Check the code/password";
+      error;
     }
   }
 
--- src/telegram-send-message.tsx
+++ src/telegram-send-message.tsx
@@ -1,69 +1,163 @@
-import {Action, ActionPanel, Form, Icon, launchCommand, LaunchType, LocalStorage, showToast, Toast} from "@raycast/api";
-import { TelegramClient } from "telegram";
+import {
+  Action,
+  ActionPanel,
+  Form,
+  Icon,
+  launchCommand,
+  LaunchType,
+  LocalStorage,
+  showToast,
+  Toast,
+  LaunchProps,
+  showHUD,
+} from "@raycast/api";
+import { useCachedPromise } from "@raycast/utils";
+import { TelegramClient, Api } from "telegram";
 import { StringSession } from "telegram/sessions";
 
-export default function Command() {
+interface Contact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber?: string;
+  username: string;
+}
+
+export default function Command({
+  launchContext,
+}: LaunchProps<{ launchContext: { id: string; firstName?: string; lastName?: string } }>) {
+  const { data: contacts, isLoading } = useCachedPromise(async () => {
+    const contacts = await LocalStorage.getItem<string>("contacts");
+    return contacts ? JSON.parse(contacts) : [];
+  });
+
   return (
     <Form
-    actions={
-      <ActionPanel>
-        <SendMessage />
-      </ActionPanel>
-    }
+      isLoading={isLoading}
+      actions={
+        <ActionPanel>
+          <SendMessage />
+          <ReceiveContacts />
+          <DeleteLocalStorage />
+        </ActionPanel>
+      }
+      enableDrafts
     >
-    <Form.TextArea id="message" title="Message" placeholder="The message you want to send" />
-    <Form.TextField id="userId" title="User ID" placeholder="The user ID of the recipient" />
+      <Form.TextArea id="message" title="Message" placeholder="The message you want to send" storeValue />
+      <Form.Dropdown id="contact" {...contacts} title="Receiver" placeholder="Select a contact" storeValue>
+        {contacts?.map((contact, index) => {
+          return (
+            <Form.Dropdown.Item
+              key={index}
+              value={contact.id}
+              title={`${contact.firstName} ${contact.lastName}`}
+              keywords={[contact.firstName, contact.lastName, contact.phoneNumber, contact.username]}
+            />
+          );
+        })}
+        ;
+      </Form.Dropdown>
     </Form>
   );
 }
 
 function SendMessage() {
-  
-  async function handleSubmit(values: { message: string, userId: string}) {
+  async function handleSubmit(values: { message: string; contact: string }) {
     if (!values.message) {
       showToast({
         style: Toast.Style.Failure,
         title: "Please enter a message",
       });
       return;
-    } else if (!values.userId) {
-      showToast({
-        style: Toast.Style.Failure,
-        title: "Please enter a user ID",
-      });
-      return;
     }
 
     const toast = await showToast({
-      style:Toast.Style.Animated,
+      style: Toast.Style.Animated,
       title: "Sending message",
     });
 
     try {
-      const savedSession:string = await LocalStorage.getItem("savedSession") || "";
+      const savedSession: string = (await LocalStorage.getItem("savedSession")) || "";
       const SESSION = new StringSession(savedSession);
 
-if (!savedSession) {
-  await launchCommand({name: "telegram-login", type: LaunchType.UserInitiated, context: {message: "Log in to begin"}});
-  return;
-}
+      if (!savedSession) {
+        await launchCommand({
+          name: "telegram-login",
+          type: LaunchType.UserInitiated,
+          context: { message: "Log in to begin" },
+        });
+        return;
+      }
 
-      const client = new TelegramClient(SESSION, 12345678, 'api_hash', { connectionRetries: 5 });
+      const client = new TelegramClient(SESSION, 12345678, "api_hash", { connectionRetries: 5 });
       await client.connect();
       const message = values.message;
-      await client.sendMessage(`${values.userId}`, {message});
-    
-    toast.style = Toast.Style.Success;
-    toast.title = "Message sent";
-    
+      await client.sendMessage(`${values.contact}`, { message });
+
+      toast.style = Toast.Style.Success;
+      toast.title = "Message sent";
     } catch (error) {
       toast.style = Toast.Style.Failure;
       toast.title = "Failed sending message";
       toast.message = String(error);
-      console.log(error);
     }
-
   }
 
-  return <Action.SubmitForm icon={Icon.Upload} title="Send message" onSubmit={handleSubmit} />;
+  return <Action.SubmitForm icon={Icon.SpeechBubbleActive} title="Send Message" onSubmit={handleSubmit} />;
+}
+
+function ReceiveContacts() {
+  async function handleSubmit() {
+    const toast = await showToast({
+      style: Toast.Style.Animated,
+      title: "Receiving contacts",
+    });
+
+    try {
+      const savedSession: string = (await LocalStorage.getItem("savedSession")) || "";
+      const SESSION = new StringSession(savedSession);
+
+      if (!savedSession) {
+        await launchCommand({
+          name: "telegram-login",
+          type: LaunchType.UserInitiated,
+          context: { message: "Log in to begin" },
+        });
+        return;
+      }
+
+      const client = new TelegramClient(SESSION, 12345678, "api_hash", { connectionRetries: 5 });
+      await client.connect();
+      const contacts = await client.invoke(new Api.contacts.GetContacts({}));
+      if (contacts instanceof Api.contacts.Contacts) {
+        const usersList: Contact[] = contacts.users.map((user) => ({
+          id: user.id.toString(), // Convert BigInt to string
+          firstName: user.firstName || "",
+          lastName: user.lastName || "",
+          phoneNumber: user.phone || "",
+          username: user.username || "",
+        }));
+
+        LocalStorage.setItem("contacts", JSON.stringify(usersList));
+      }
+
+      toast.style = Toast.Style.Success;
+      toast.title = "Contacts updated";
+    } catch (error) {
+      toast.style = Toast.Style.Failure;
+      toast.title = "Failed receiving contacts";
+      toast.message = String(error);
+    }
+  }
+
+  return <Action.SubmitForm icon={Icon.RotateAntiClockwise} title="Refresh Contacts" onSubmit={handleSubmit} />;
+}
+function DeleteLocalStorage() {
+  async function handleSubmit() {
+    await LocalStorage.clear();
+    await showHUD("Telegram Raycast Cleared!");
+    return;
+  }
+
+  return <Action.SubmitForm icon={Icon.Xmark} title="Reset Extension" onSubmit={handleSubmit} />;
 }
